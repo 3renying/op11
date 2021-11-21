59ff5b2350584aa17deaa0ad041664c7404f9ac6
3renying <76834403+3renying@users.noreply.github.com>
2021-11-21 10:19:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,5 @@
 # Add a feed source
  echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
  echo 'src-git passwall https://github.com/kenzok8/small' >>feeds.conf.default
- git clone https://github.com/3renying/kenzok8-openwrt-packages/tree/master/luci-app-passwall package/luci-app-passwall
+ echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+ 
